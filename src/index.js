f27014771355bb2e4888eda5ee1d9814a1243a52
Jonathan Caufield <joncaufield@gmail.com>
2020-10-11 09:07:26 -0600

    npm start command works with sample app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("button");
+const output = document.getElementById("output");
+
+button.addEventListener("click", () => {
+  output.innerText = "Thanks! ^.^";
+});
