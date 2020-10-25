cfe18b6947eb471131ee1a7ae410d9f53851a596
Jonathan Caufield <joncaufield@gmail.com>
2020-10-25 08:49:24 -0600

    delaunay triangulation possible

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
+import {points, Delaunate} from './delaunay/delaunay'
+
 const button = document.getElementById("button");
 const output = document.getElementById("output");
 
 button.addEventListener("click", () => {
-  output.innerText = "Thanks! ^.^";
+  //output.innerText = "Thanks! ^.^";
+  var delaunayObject = Delaunate(points);
+  var outputString = 'Points Supplied: [' + points[0] +']';
+  
+
+  for (var i = 1; i < points.length; i++)
+  {
+    outputString += (', [' + points[i] + ']');
+  }
+  
+  outputString += '\nDelaunay Object: ' + delaunayObject.triangles;
+
+  outputString += '\nInterpretation: '
+
+  for (var i = 0; i < delaunayObject.triangles.length; i += 3)
+  {
+    outputString += '\nTriangle ' + Math.floor(i/3) + ': ';
+    outputString += ('[' + points[delaunayObject.triangles[i]] + ']');
+    outputString += (', [' + points[delaunayObject.triangles[i+1]] + ']');
+    outputString += (', [' + points[delaunayObject.triangles[i+2]] + ']');
+  }
+
+  output.innerText = outputString;
 });
