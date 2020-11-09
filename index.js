d66ed21688fba36adb0463ee8e785824ca5eba8c
Derek <Derek_05@outlokk.com>
2020-11-09 09:20:57 -0800

    function to produce selected options

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const output = document.getElementById("output");
 
 button.addEventListener("click", function() {
     var ran = Math.floor(Math.random() * 1000000000)
-    var type = document.getElementById('type1')
-    var reg = document.getElementById('Regional1')
-    var water = document.getElementById('water1')
+    var type = document.getElementById('type1').selectedIndex;
+    var reg = document.getElementById('Regional1').selectedIndex;
+    var water = document.getElementById('water1').selectedIndex;
     output.innerText = ran + type + reg + water;
 
   });
