8c51f449300e7f0514e197b37eed9d78bb331b76
Derek <Derek_05@outlokk.com>
2020-11-26 12:37:59 -0800

    testing

---
+++ GenerateName.js
@@ -0,0 +1,180 @@
+class GenerateName {
+    constructor(){
+    this.reg = 0;
+    this.nameArr = [];
+    this.ranNum1 = [];
+    this.ranNum2 = [];
+
+    }
+
+
+// to take the string Regional from the html and produce the number to fill in reg variable.
+     getReg(x){
+
+        var tempReg = x;
+        //tempReg = document.getElementById('Regional1').value;
+
+        if(tempReg == "Random")
+        {
+            this.changeRandom(tempReg);
+        }
+        else if(tempReg == "1"){
+            this.reg = 1;
+        }
+        else if(tempReg == "2"){
+            this.reg = 2;
+        }
+        else if(tempReg == "3"){
+            this.reg = 3;
+        }
+        else if(tempReg == "4"){
+            this.reg = 4;
+        }
+        else if(tempReg == "5"){
+            this.reg = 5;
+        }
+        else {
+            this.reg = 6;
+        }
+
+        
+        
+
+
+    }
+
+// if the user chooses random for Regional then this will produce a random number between 1 - 6.
+     changeRandom(){
+        
+            var x = (Math.random() * 100) % 6;
+            this.reg = Math.round(x);
+ 
+    }
+
+
+    
+}
+
+ class WorldNames extends GenerateName {
+
+    getNames() {
+
+        var x = this.reg;
+        var temp;
+
+        this.createRanNum();
+
+        for (var i = 0; i < x; i++){
+
+            temp = countries[ranNum1[i]];
+            this.nameArr[i] =  temp;
+        }
+
+        
+
+    }
+
+    // creating random numbers for reg names
+    createRanNum(){
+
+        var x = this.reg;
+        var y = countries.length;
+
+        for (var i = 0; i < x; i++)
+        {
+            this.ranNum[i] = Math.random() % y;
+        }
+
+        
+
+        
+    }
+
+
+}
+
+ class CounrtyNames extends GenerateName {
+
+    getNames() {
+
+        var x = this.reg;
+        var temp;
+
+        this.createRanNum();
+
+        for (var i = 0; i < x; i++){
+
+            temp = provinces[ranNum1[i]];
+            this.nameArr[i] =  temp;
+        }
+
+        
+
+    }
+
+    // creating random numbers for reg names
+    createRanNum(){
+
+        var x = this.reg;
+        var y = provinces.length;
+
+        for (var i = 0; i < x; i++)
+        {
+            this.ranNum1[i] = Math.random() % y;
+        }
+
+        
+
+        
+    }
+}
+
+
+
+
+ class KingdomNames extends GenerateName {
+
+    getNames(){
+
+        var x = this.reg;
+        var temp;
+
+        this.createRanNum();
+
+        for (var i = 0; i < x; i++){
+
+            temp = citiesPrefix[ranNum1[i]] + citiesSuffix[ranNum2[i]];
+            this.nameArr[i] =  temp;
+        }
+
+        
+    }
+
+    // creating random numbers for reg names
+    createRanNum(){
+
+        // for prfix
+        var x = this.reg;
+        var y = citiesPrefix.length;
+
+        for (var i = 0; i < x; i++)
+        {
+            this.ranNum1[i] = Math.random() % y;
+        }
+
+        // for suffix
+        var x = this.reg;
+        var y = citiesSuffix.length;
+
+        for (var i = 0; i < x; i++)
+        {
+            this.ranNum2[i] = Math.random() % y;
+        }
+
+        
+        
+    }
+        
+
+
+ }
--- index.js
+++ index.js
@@ -2,10 +2,108 @@ const button = document.getElementById("button");
 const output = document.getElementById("output");
 
 button.addEventListener("click", function() {
-    var ran = Math.floor(Math.random() * 1000000000)
-    var type = document.getElementById('type1').value
-    var reg = document.getElementById('Regional1').value
-    var water = document.getElementById('water1').value
-    output.innerText = ran + " " + type + " " + reg + " " + water;
+
+
+ 
+
+  // getting the values from html and checking if the user entered a seed number.
+    var seed = document.getElementById('Seed').value;
+
+
+    if(seed == null || seed == ""){
+    
+        seed = Math.round(Math.floor(Math.random() * 1000000000))
+
+    }
+
+    if(seed.toString().length > 9 || seed.toString().length < 9){
+
+      output.innerText ="The seed you entered is incorrect, can only be a 9 digit number.";
+
+    }
+
+    else{
+      // checking if type of map is world, country, kingdom or random, if random pick one of the three
+      var type = document.getElementById('type1').value
+      var reg = document.getElementById('Regional1').value
+      var water = document.getElementById('water1').value
+
+      if(type == "Random"){
+        var x = Math.random() % 3;
+        x = Math.random(x);
+        if(type == 1){
+          type = "world";
+        }
+        else if(x==2){
+          type = "country";
+        }
+        else{
+          type = "kingdom";
+        }
+      }
+
+      //calling class for the generated names
+      if(type == "world"){
+
+        let worldNam = new WorldNames();
+        worldNam.getReg(reg);
+        worldNam.getNames();
+        reg = worldNam.reg;
+
+      }
+      else if(type == "country"){
+
+        let countryNam = new CountryNames();
+        countryNam.getReg(reg);
+        countryNam.getNames();
+        reg = countryNam.reg;
+
+      }
+      else{
+
+        let kingdomNam = new KingdomNames();
+        kingdomNam.getReg(reg);
+        kingdomNam.getNames();
+        reg = kingdomNam.reg;
+
+      }
+
+      // turning the type into numbers value
+      if(type == "world"){
+
+        type = 1;
+      }
+      else if(type == "country"){
+
+        type = 2;
+      }
+      else{
+        type = 3;
+      }
+
+      //turning water into number value
+      if(water == "none"){
+
+        water = 0;
+      }
+      else if(water == "alittle"){
+
+        water = 1;
+      }
+      else if(water == "moderate") {
+        water = 2;
+      }
+      else{
+        water = 3;
+      }
+
+
+
+
+      output.innerText = seed + " " + type + " " + reg + " " + water;
+
+    }
 
   });
+
+  
