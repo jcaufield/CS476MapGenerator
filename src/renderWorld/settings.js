83697b9efd7fdcf1cb2374df8591a93d0fd95a92
Jonathan Caufield <joncaufield@gmail.com>
2020-11-29 10:33:02 -0600

    pivoting to legend strategy

--- src/renderWorld/settings.js
+++ src/renderWorld/settings.js
@@ -1,3 +1,4 @@
 const colors = ['#fc8d62', '#8da0cb', '#66c2a5', '#e78ac3', '#a6d854', '#1bc0c9']; //colors in order of inclusion
-const anchors = [{x: .2, y: .4}, {x: .75, y: .8}, {x: .5, y: .45}, {x: .4, y: .9}, {x: .8, y: .15}, {x: .95, y: .5}]; // where to look for region start points
-const pointCount = [100, 350, 500]; //number of points to take from generated points
+//const anchors = [{x: .2, y: .4}, {x: .65, y: .8}, {x: .5, y: .45}, {x: .4, y: .9}, {x: .8, y: .15}, {x: .9, y: .5}]; // where to look for region start points
+const anchors = [{x: .35, y: .5},{x: .55, y: .15},{x: .7, y: .75},{x: .1, y: .25},{x: .1, y: .8},{x: .85, y: .5}]
+const pointCount = [150, 300, 500]; //number of points to take from generated points
